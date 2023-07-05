5345969481ae4028406301e9d2f5a1d36c99db86
Zhydani <zhydani@gmail.com>
2023-07-05 20:13:03 -0300

    adicionando botao de checked

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,6 +1,6 @@
 import styles from './Task.module.css'
-
-import { Trash, Circle } from 'phosphor-react'
+import { useState } from 'react'
+import { Trash, Circle, CheckCircle } from 'phosphor-react'
 
 interface TaskProps {
   content: string
@@ -9,16 +9,30 @@ interface TaskProps {
 
 export function Task({ content, onDeleteTask }: TaskProps) {
 
+  const [isChecked, setIsChecked] = useState(false);
+
   function handleDeleteTask() {
     onDeleteTask(content)
+  }
+
+  function handleCheckTask() {
+    const check = isChecked
+    if (check) {
+      setIsChecked(false)
+    }else{
+      setIsChecked(true)
+    }
   }
 
   return (
     <div className={styles.task}>
       <div className={styles.taskBox}>
         <div className={styles.taskContent}>
-          <button title="Concluir tarefa" className={styles.taskButton}>
-            <Circle size={24} />
+          <button onClick={handleCheckTask} title="Concluir tarefa" className={styles.checkButton}>
+            {isChecked ? 
+              <CheckCircle weight='fill' size={24} className={styles.checkCircle} /> :
+              <Circle size={24} className={styles.circle} />
+            }
           </button>
           <p>{content}</p>
           <button onClick={handleDeleteTask} title="Deletar tarefa" className={styles.deleteButton}>
